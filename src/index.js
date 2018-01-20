afb5128e8ceec99d0b07c554bde29dc5d9cd0c56
hepiska <ego1403@gmail.com>
2018-01-20 22:18:40 +0700

    add deploy script

--- src/index.js
+++ src/index.js
@@ -36,14 +36,5 @@ ReactDOM.render((
 
 registerServiceWorker()
 requestPermission().then(res => {
-  console.log(res);
+  localStorage.setItem('pushNotifToken', res.token);
 })
-
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('../firebase-messaging-sw.js')
-//   .then(function(registration) {
-//     console.log('Registration successful, scope is:', registration.scope);
-//   }).catch(function(err) {
-//     console.log('Service worker registration failed, error:', err);
-//   });
-// }
