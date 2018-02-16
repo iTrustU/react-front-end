2e0d67e2ad14c3d4e7f051735236bdc477ad6635
hepiska <ego1403@gmail.com>
2018-02-16 21:19:18 +0700

    logo to home

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -35,10 +35,12 @@ class  Header extends Component {
           <Icon size='large' color='orange' name='content'/>
         </MenuContainer>
         <LogoContainer>
+          <Link to='/'>
           <img
             style={{width:100}}
             src='https://firebasestorage.googleapis.com/v0/b/itrustu-a10b5.appspot.com/o/ItrustU-text-logo.png?alt=media&token=bfa1b4eb-f1a0-4202-a846-76ebc0253a83'/>
-        </LogoContainer>
+        </Link>
+      </LogoContainer>
         <MenuContainer onClick={this.changeIsSearch} >
           <Icon size='large' color='orange' name='search'/>
         </MenuContainer>
